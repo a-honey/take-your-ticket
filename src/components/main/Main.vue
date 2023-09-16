97d6144afd2108360bbe8fcb85c44d566702829e
a-honey <jungahyeon0512@khu.ac.kr>
2023-09-16 16:42:10 +0900

    :ambulance: Fix: 이미지 URL 전달 오류로 인한 싱글 페이지 애플리케이션 변경

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -2,7 +2,7 @@
   <header>
     <h1>Take Your Ticket</h1>
   </header>
-  <main>
+  <main v-if="isUploading === true">
     <form @submit="handleSubmit">
       <div class="formContainer">
         <div class="imgFormContainer">
@@ -39,6 +39,37 @@
       </div>
     </form>
   </main>
+  <main class="isUploading" v-if="isUploading === false">
+    <div class="Container">
+      <div class="imgContainer">
+        <img :src="img" alt="" />
+      </div>
+      <div class="ticketContainer">
+        <label class="items logo">TAKE YOUR TICKET</label>
+        <div class="items title">{{ title }}</div>
+        <div class="items info">{{ info }}</div>
+        <div class="items rating">
+          <img
+            v-for="a in rating"
+            :key="a"
+            src="/start.png"
+            alt="rating_star"
+          />
+        </div>
+        <div class="items review">{{ review }}</div>
+        <div class="results items">
+          <div>interested</div>
+          <div>4.5</div>
+          <div>춤</div>
+        </div>
+      </div>
+    </div>
+    <div class="btns">
+      <button>Save</button>
+      <button @click="handleBackClick">Back</button>
+      <button @click="handleNewClick">New</button>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -49,6 +80,7 @@ export default {
     const searchParams = new URLSearchParams(this.$route.query);
 
     return {
+      isUploading: true,
       img: searchParams.get("img") || "",
       title: searchParams.get("title") || "",
       info: searchParams.get("info") || "",
@@ -95,9 +127,7 @@ export default {
         });
 
       // 유저가 입력한 데이터를 URL로 담아 전달
-      this.$router.push(
-        `/ticket?title=${this.title}&img=${this.img}&rating=${this.rating}&info=${this.info}&review=${this.review}`
-      );
+      this.isUploading = false;
     },
     handleImgChange(e) {
       e.preventDefault();
@@ -150,6 +180,17 @@ export default {
         .catch((error) => {
           console.error(error);
         });
+    },
+    handleNewClick() {
+      this.img = "";
+      this.title = "";
+      this.info = "";
+      this.rating = "";
+      this.review = "";
+      this.isUploading = true;
+    },
+    handleBackClick() {
+      this.isUploading = true;
     },
   },
   mounted() {
@@ -232,5 +273,69 @@ button {
 .btns {
   display: flex;
   gap: 20px;
+}
+
+/* v-if isUploading === false */
+
+main.isUploading {
+  flex-direction: column;
+  gap: 40px;
+}
+.Container {
+  display: flex;
+  gap: 40px;
+}
+.ticketContainer,
+.imgContainer {
+  width: 400px;
+  height: 600px;
+  padding: 20px;
+  background-color: #ededed;
+}
+
+.imgContainer > img {
+  width: 100%;
+  height: 100%;
+}
+
+.ticketContainer {
+  display: grid;
+  grid-template-rows: 0.5fr 1fr 1fr 1fr 2fr 1fr;
+}
+
+.items {
+  border-bottom: 2px solid black;
+  display: flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  justify-content: left;
+  font-weight: 700;
+}
+.title {
+  font-size: 20px;
+  font-weight: 700;
+}
+.rating > img {
+  width: 40px;
+  height: 40px;
+}
+.results {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+.results > div {
+  width: 100%;
+  height: 100%;
+  border-right: 2px solid #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.results > div:last-child {
+  border-right: none;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 import MainPage from "@/components/main/Main";
-import TicketPage from "@/components/ticket/Ticket";
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [
-  { path: "/", name: "MainPage", component: MainPage },
-  { path: "/ticket", name: "TicketPage", component: TicketPage },
-];
+const routes = [{ path: "/", name: "MainPage", component: MainPage }];
 const router = createRouter({
   history: createWebHistory(),
   routes,
